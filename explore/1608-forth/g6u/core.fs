085a02480140389757fbd89541663f7d8c4fb166
Jean-Claude Wippler <jc@wippler.nl>
2017-08-29 22:05:54 +0200

    add quotations to core

--- explore/1608-forth/g6u/core.fs
+++ explore/1608-forth/g6u/core.fs
@@ -7,6 +7,7 @@ compiletoflash
 include ../flib/i2c/ssd1306.fs
 include ../flib/mecrisp/graphics.fs
 include ../flib/any/digits.fs
+include ../flib/mecrisp/quotation.fs
 include ../flib/mecrisp/multi.fs
 include ../flib/any/timed.fs
 
--- explore/1608-forth/jz4/core.fs
+++ explore/1608-forth/jz4/core.fs
@@ -9,6 +9,7 @@ include ../flib/any/varint.fs
 include ../flib/i2c/ssd1306.fs
 include ../flib/mecrisp/graphics.fs
 include ../flib/any/digits.fs
+include ../flib/mecrisp/quotation.fs
 include ../flib/mecrisp/multi.fs
 
 ( core end, size: ) here dup hex. swap - .
