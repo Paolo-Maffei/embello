c2c11355342a8db99a6f56960920fa681bc9d481
Jean-Claude Wippler <jc@wippler.nl>
2017-10-22 15:20:58 +0200

    add eeprom programming example

---
+++ explore/1608-forth/jz4/ex/eeprom.fs
@@ -0,0 +1,28 @@
+\ eeprom access
+
+compiletoram? [if]  forgetram  [then]
+
+\ $40022000 constant FLASH
+   FLASH $04 + constant FLASH-PECR
+   FLASH $0C + constant FLASH-PEKEYR
+   FLASH $18 + constant FLASH-SR
+
+$08080000 constant EE-BASE
+
+: ee! ( u a -- )
+  begin FLASH-SR %1 and 0= until
+  FLASH-PECR @ %1 and if
+    $89ABCDEF FLASH-PEKEYR !
+    $02030405 FLASH-PEKEYR !
+  then
+  FLASH-PECR @ hex.
+  !
+  begin FLASH-SR @ %1 and 0= until
+  %1 FLASH-PECR bis!
+;
+
+EE-BASE 16 dump
+
+$11223344 EE-BASE 8 + ee!
+
+EE-BASE 16 dump
