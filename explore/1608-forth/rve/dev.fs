30d75e9e3487b96fcc683cb89dc684c8310e8c47
Jean-Claude Wippler <jc@wippler.nl>
2018-02-09 23:36:56 +0100

    more code, reorgs, and fixes

--- explore/1608-forth/rve/dev.fs
+++ explore/1608-forth/rve/dev.fs
@@ -5,59 +5,60 @@ forgetram
 \ sd-mount.
 \ ls
 
-8192 buffer: M       \ simulated memory
-ROM M ROM-SIZE move  \ initial contents
+8192 buffer: mem       \ simulated memory
+ROM mem ROM-SIZE move  \ initial contents
 
-0 variable AC
-0 variable PC
-0 variable IR
-0 variable MQ
+0 variable ac.r
+0 variable pc.r
+0 variable mq.r
+0 variable sr.r
 
 : octal ( --) #8 base ! ;  octal  \ switch to octal mode from here on down
+: o.4 ( u -- ) base @ octal swap  u.4  base ! ;
 
-: m@ ( u -- u )  shl M + h@ ;
-: m! ( u1 u2 -- )  shl M + h! ;
-: ac@ ( -- u )  AC @ ;
-: ac! ( u -- )  AC ! ;
-: pc@ ( -- u )  PC @ ;
-: pc! ( u -- )  PC ! ;
+: m@ ( u -- u )  shl mem + h@ ;
+: m! ( u1 u2 -- )  shl mem + h! ;
+: ac  ( -- u )  ac.r @ ;
+: ac! ( u -- )  ac.r ! ;
+: pc  ( -- u )  pc.r @ ;
+: pc! ( u -- )  pc.r ! ;
 
 : maskw ( u -- u ) 07777 and ;
 : maskl ( u -- u ) 17777 and ;
 : keepl ( u -- u ) 10000 and ;
 : 1+w   ( u -- u ) 1+ maskw ;
-: ++pc  ( -- )     pc@ 1+w pc! ;
+: ++pc  ( -- )     pc 1+w pc! ;
 
 : addr ( u -- u )
-  dup 0177 and  \ immediate
+  dup 0177 and  ( ir ac )  \ immediate
   over 0200 and if
-    pc@ 1- 7600 or  \ page-relative
+    pc 1- 7600 and or  \ page-relative
   then
   swap 0400 and if
     dup 7770 and 0010 = if dup m@ 1+w over m! then  \ auto-inc
     m@  \ indirect
   then ;
 
-: op0 ( u -- ) addr m@  10000 or  ac@ and ac! ;              \ AND
-: op1 ( u -- ) addr m@  ac@ + maskl ac! ;                    \ TAD
-: op2 ( u -- ) addr  dup m@ 1+w  dup rot m!  if ++pc then ;  \ ISZ
-: op3 ( u -- ) addr  ac@  dup maskw  rot m!  keepl ac! ;     \ DCA
-: op4 ( u -- ) addr  pc@ over m!  1+w pc! ;                  \ JMS
-: op5 ( u -- ) addr pc!  ;                                   \ JMP
+: op0 ( u -- ) addr m@  10000 or  ac and ac! ;                  \ AND
+: op1 ( u -- ) addr m@  ac + maskl ac! ;                        \ TAD
+: op2 ( u -- ) addr  dup m@ 1+w  dup rot m!  0= if ++pc then ;  \ ISZ
+: op3 ( u -- ) addr  ac  dup maskw  rot m!  keepl ac! ;         \ DCA
+: op4 ( u -- ) addr  pc over m!  1+w pc! ;                      \ JMS
+: op5 ( u -- ) addr pc!  ;                                      \ JMP
 
 : op6 ( u -- )  \ IOT
     dup  3 rshift 077 and case
       03 of dup 1 and if key? if ++pc then then  \ skip if input ready
-                4 and if key? if key else 0 then ac@ keepl or ac! then  \ rdch
+                4 and if key? if key else 0 then ac keepl or ac! then  \ rdch
       endof
       04 of dup 1 and if ++pc then  \ skip if output ready
-            dup 4 and if ac@ 0177 and emit then  \ wrch
-                2 and if ac@ keepl ac! then  \ clear flag
+            dup 4 and if ac 0177 and emit then  \ wrch
+                2 and if ac keepl ac! then  \ clear flag
       endof
     endcase  drop ;
 
 : op7g1 ( u -- )  \ OPR
-  ac@  ( ir ac )
+  ac  ( ir ac )
   over 0200 and if     keepl then  \ CLA
   over 0100 and if     maskw then  \ CLL
   over 0040 and if 07777 xor then  \ CMA
@@ -73,13 +74,22 @@ ROM M ROM-SIZE move  \ initial contents
   ac! ;
 
 : op7g2 ( u -- )  \ OPR
+  1  ( ir f )
+  over 0100 and if ac 04000 and if shr then then  \ SMA, SPA
+  over 0040 and if ac maskw 0=  if shr then then  \ SZA, SNA
+  over 0020 and if ac maskl     if shr then then  \ SNL, SZL
+  over 0010 and if                 1 xor    then  \ reverse
+  0= if ++pc then  \ skip if above condition is met
+  dup 0200 and if ac keepl     ac! then  \ CLA
+  dup 0004 and if ac sr.r @ or ac! then  \ OSR
+      0002 and if cr ." HALT" quit then  \ HLT
 ;
 
 : op7g3 ( u -- )  \ OPR
-  MQ @ swap  ( mq ir )
-  dup 0200 and if ac@ keepl ac! then  \ CLA
-  dup 0020 and if ac@ dup maskw MQ ! keepl ac! then  \ MQL
-      0100 and if ac@ or ac! else drop then ;
+  mq.r @ swap  ( mq ir )
+  dup 0200 and if ac                  keepl ac! then  \ CLA
+  dup 0020 and if ac dup maskw mq.r ! keepl ac! then  \ MQL
+      0100 and if ac or ac! else drop then ;
 
 : op7 ( u -- )  \ OPR
   dup 0400 and 0= if op7g1 else
@@ -89,9 +99,18 @@ ROM M ROM-SIZE move  \ initial contents
 create op-tab ' op0 , ' op1 , ' op2 , ' op3 , ' op4 , ' op5 , ' op6 , ' op7 , 
 
 : cycle
-  pc@  dup 1+w pc!  m@
-  dup #8 rshift %1100 and  op-tab + @  execute ;
+  pc  dup 1+w pc!  m@
+  dup 7 rshift %11100 and  op-tab + @  execute ;
 
 0200 pc!
 
 decimal
+
+: run
+  100000 0 do
+    \ cr ." pc " pc o.4 ." : " pc m@ o.4 ."  ac " ac o.4 space
+    cycle
+  loop ;
+
+run
+.s
