66d896538cbc43f71063ba258c0e27f46f2eb095
Jean-Claude Wippler <jc@wippler.nl>
2018-02-13 15:17:43 +0100

    remove unused code, core.fs is no longer needed

--- explore/1608-forth/bp8/core.fs
+++ explore/1608-forth/bp8/core.fs
@@ -4,15 +4,15 @@
 compiletoflash
 ( core start: ) here hex.
 
-include ../flib/mecrisp/quotation.fs
-include ../flib/mecrisp/multi.fs
-include ../flib/any/timed.fs
+\ include ../flib/mecrisp/quotation.fs
+\ include ../flib/mecrisp/multi.fs
+\ include ../flib/any/timed.fs
 
-PC13 constant LED
+\ PC13 constant LED
 
-: led-init  omode-pp LED io-mode!  LED ios! ;
-: led-on  LED ioc! ;
-: led-off  LED ios! ;
+\ : led-init  omode-pp LED io-mode!  LED ios! ;
+\ : led-on  LED ioc! ;
+\ : led-off  LED ios! ;
 
 cornerstone <<<core>>>
 hello
